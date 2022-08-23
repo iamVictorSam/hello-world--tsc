742920a73637c144e65200fc081a8bcc330eede6
IamVictorSam <iamvictorsam@gmail.com>
2022-08-23 05:34:52 +0100

    understanding arrays

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,11 @@ const getAge = (year: any)=>{
 }
 
 //n.b function parameters requires a type. if you dont want to specify, use the any type
+
+
+//Arrays
+//when declaring arrays in js, if the array is empty it is of **any** type (try avoiding this... if the array is gonna be empty, specify a type for it). But if the array contains numbers or strings, or any other type, it takes the type from the value
+
+const ages: number[] = [];
+
+const names = ['sam', 'victor', 'jess']
