c5824f4ab22800caaf6aeb380af52d2864fab122
IamVictorSam <iamvictorsam@gmail.com>
2022-08-23 08:05:50 +0100

    understanding union type

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,16 @@ const employee: Employee = {
   name: "samflash",
   retirement: (date) => console.log(date),
 };
+
+
+// Union Types
+// With union types, we can give a varible or function param more than 1 type
+
+function kgToLbs(weight: number | string) : number {
+    // Now we'll do Narrowing(creating possible instances for each type)
+    if (typeof weight === 'number')
+        return weight * 42.3
+    else 
+        return parseInt(weight) * 32.3
+}
+
